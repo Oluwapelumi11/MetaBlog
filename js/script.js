7c85fc5e80a50fd511de6268394bb7968533534e
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2023-11-05 01:26:52 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const toggle=document.querySelector('#toggle')
+const html = document.documentElement;
+
+
+
+toggle.addEventListener('change',function() {
+//    dark mode logic here
+//     Dont know why my method isn't working, you can try to fix it
+    
+});
